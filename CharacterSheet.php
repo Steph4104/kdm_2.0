2e700112c89bb075d517340ceb9c26398d641a4e
Stephanie <stephanie4104@gmail.com>
2017-12-09 11:05:31 -0500

    seperate survival

--- CharacterSheet.php
+++ CharacterSheet.php
@@ -121,43 +121,7 @@ $w_proficiency = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM w_proficie
     	</div>
 		<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
       		<div class="panel-body">
-        		 <div class="col-sm-4 ">
-                 	<label for="SurvivalPts"  class="col-sm-5">Survival Points: </label>
-                        <div class="col-sm-3 ">
-                            <input type="SurvivalPts" class="form-control" id="SurvivalPts" placeholder=" # " value=<?php echo $survivol['SURVIVOL'];?> >
-                        </div>
-                 </div>
-                  <div class="col-sm-2">
-                 	<div class="checkbox">
-                        <label>
-                            <input type="checkbox"  value="Dodge" id="SurvivalDodge"> Dodge
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"  value="Dash" id="SurvivalDash"> Dash
-                        </label>
-                    </div>
-                 </div>
-                 <div class="col-sm-2">
-                 	<div class="checkbox">
-                        <label>
-                            <input type="checkbox"  value="Encourage" id="SurvivalEncourage"> Encourage
-                        </label>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"  value="Surge" id="SurvivalSurge"> Surge
-                        </label>
-                    </div>
-                 </div>
-                 <div class="col-sm-4">
-                 	<div class="checkbox">
-                        <label>
-                            <input type="checkbox"  value="NoSurvival" id="NoSurvival"> Cannot spend survivals.
-                        </label>
-                    </div>
-            	</div>        
+              <?php require_once 'survivals.php'; ?>
       		</div>
     	</div>
 	</div>
---
+++ survivals.php
@@ -0,0 +1,37 @@
+<div class="col-sm-4 ">
+    <label for="SurvivalPts"  class="col-sm-5">Survival Points: </label>
+    <div class="col-sm-3 ">
+        <input type="SurvivalPts" class="form-control" id="SurvivalPts" placeholder=" # " value=<?php echo $survivol['SURVIVOL'];?> >
+    </div>
+</div>
+<div class="col-sm-2">
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"  value="Dodge" id="SurvivalDodge"> Dodge
+        </label>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"  value="Dash" id="SurvivalDash"> Dash
+        </label>
+    </div>
+</div>
+<div class="col-sm-2">
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"  value="Encourage" id="SurvivalEncourage"> Encourage
+        </label>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"  value="Surge" id="SurvivalSurge"> Surge
+        </label>
+    </div>
+</div>
+<div class="col-sm-4">
+    <div class="checkbox">
+        <label>
+            <input type="checkbox"  value="NoSurvival" id="NoSurvival"> Cannot spend survivals.
+        </label>
+    </div>
+</div>        
