5f702da99585a2aab00152298d5c516eb895f8a4
Stephanie <stephanie4104@gmail.com>
2018-03-04 13:38:30 -0500

    Update disorder ajax

--- disorders.php
+++ disorders.php
@@ -6,20 +6,18 @@ $query =  "SELECT * FROM fighting_disroder WHERE ID_SURVIVOR = $survivor_id";
 
 if ($disorder = $con->query($query)) {
 
-      while ($row = $disorder->fetch_assoc()) {
-        if($row['NUM_DISORDER'] == 1){
-          $disorder_num1 = $row["ID_DISORDER"];
-        }
-
-        if($row['NUM_DISORDER'] == 2){
-          $disorder_num2 = $row["ID_DISORDER"];
-        }
-
-        if($row['NUM_DISORDER'] == 3){
-          $disorder_num3 = $row["ID_DISORDER"];
-        }
-          
-      }
+  while ($row = $disorder->fetch_assoc()) {
+    if($row['TYPE'] == 'disorder' && $row['POSITION'] == 1){
+      $disorder_num1 = $row["ID_ACTION"];
+    }
+    if($row['TYPE'] == 'disorder' && $row['POSITION'] == 2){
+      $disorder_num2 = $row["ID_ACTION"];
+    }
+    if($row['TYPE'] == 'disorder' && $row['POSITION'] == 3){
+      $disorder_num3 = $row["ID_ACTION"];
+    }
+      
+  }
   
   }
 
@@ -33,7 +31,7 @@ if ($disorder = $con->query($query)) {
 <?php 
 
 ?>
-<select class="form-control">
+<select class="form-control update_fighting" id='disorder_1'>
 <?php 
 
 $disorder_desc = mysqli_query($con, "SELECT * FROM disorder ORDER BY ID ASC");  
@@ -49,14 +47,16 @@ while ($row = $disorder_desc->fetch_assoc()) {
 
 ?>
 </select>
-    <label for="fighting_art_1">#1</label>
-    <textarea class="form-control" id="fighting_art_1" rows="3"><?php echo (isset($disorder_description1) ? $disorder_description1 : ''); ?></textarea>
-  </div>
-
+    <label for="disorder_1">#1</label>
+    <div id="update_disorder_1">
+    <textarea class="form-control" id="disorder_1" rows="3"><?php echo (isset($disorder_description1) ? $disorder_description1 : ''); ?></textarea>
+  
+    </div>
+    </div>
   <div class="form-group col-sm-4">
   <br/>
   <label >Disorder: </label>
-  <select class="form-control">
+  <select class="form-control update_fighting" id='disorder_2'>
   <?php 
     $disorder_desc = mysqli_query($con, "SELECT * FROM disorder ORDER BY ID ASC");  
    
@@ -71,14 +71,16 @@ while ($row = $disorder_desc->fetch_assoc()) {
   }
   ?>
   </select>
-      <label for="fighting_art_1">#2</label>
-      <textarea class="form-control" id="fighting_art_1" rows="3"><?php echo (isset($disorder_description2) ? $disorder_description2 : ''); ?></textarea>
+      <label for="disorder_1">#2</label>
+      <div id="update_disorder_2">
+      <textarea class="form-control" id="disorder_2" rows="3"><?php echo (isset($disorder_description2) ? $disorder_description2 : ''); ?></textarea>
+    </div>
     </div>
 
     <div class="form-group col-sm-4">
     <br/>
-    <label >Fighting art: </label>
-    <select class="form-control">
+    <label> Disorder: </label>
+    <select class="form-control update_fighting" id='disorder_3'>
     <?php 
     $disorder_desc = mysqli_query($con, "SELECT * FROM disorder ORDER BY ID ASC");  
     while ($row = $disorder_desc->fetch_assoc()) {
@@ -95,7 +97,31 @@ while ($row = $disorder_desc->fetch_assoc()) {
     ?>
 
     </select>
-        <label for="fighting_art_1">#3</label>
-        <textarea class="form-control" id="fighting_art_1" rows="3"><?php echo (isset($disorder_description3) ? $disorder_description3 : ''); ?></textarea>
+        <label for="disorder_3">#3</label>
+        <div id="update_disorder_3">
+        <textarea class="form-control" id="disorder_3" rows="3"><?php echo (isset($disorder_description3) ? $disorder_description3 : ''); ?></textarea>
+      </div>
       </div>
       		</div>
+
+          <script>
+          $(document).ready(function() {
+ $('.update_fighting').off().on('change', function() {
+  var itemVal = $(this).val();
+  var character = <?php echo $survivor_id; ?>;
+  var itemID = $(this).attr('id');
+  alert('select disorder change');
+
+$.ajax({
+  url: "update_fight.php",
+  type: 'POST',
+  data: { value: itemVal, id : character, type: itemID} ,
+}).success(function(data){
+
+  var section = '#update_' + itemID;
+  console.log(section);
+  $(section).html(data);
+});
+});
+          });
+</script>
--- update_fight.php
+++ update_fight.php
@@ -5,7 +5,7 @@ require_once 'database.php';
 $con=mysqli_connect($db_host, $db_username, $db_password,$db_name);
 $survivor_id = $_POST['id'];
 
-//if($_POST['type'] == 'fighting_art_1'){
+if($_POST['type'] == 'fighting_art_1' || $_POST['type'] == 'fighting_art_2' || $_POST['type'] == 'fighting_art_3'){
 
 	switch ($_POST['type']) {
 		case 'fighting_art_1':
@@ -20,13 +20,13 @@ $survivor_id = $_POST['id'];
 	}
 $value = $_POST['value'];
 
-$fighting_art = mysqli_query($con, "SELECT id FROM fighting_disroder WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."'");  
+$fighting_art = mysqli_query($con, "SELECT id FROM fighting_disroder WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."' AND TYPE ='disorder'");  
 
 if($fighting_art->num_rows == 0){
 	$sql = "INSERT INTO fighting_disroder (ID_SURVIVOR, TYPE, ID_ACTION,POSITION) VALUES (".$survivor_id.",'fighting', ".$value.", ".$position.")";
 	
 }else{
-	$sql = "UPDATE fighting_disroder SET ID_ACTION ='".$value."' WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."'";
+	$sql = "UPDATE fighting_disroder SET ID_ACTION ='".$value."' WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."' AND TYPE ='disorder'";
 	
 }
 
@@ -42,7 +42,44 @@ if($fighting_art->num_rows == 0){
         
     echo '<textarea class="form-control" id="textarea_fighting_art_1" rows="3">'.(isset($fighting_description1['Rules']) ? $fighting_description1['Rules'] : '').'</textarea>';
 	error_log($fighting_description1['Rules']);
-//}
+} else if($_POST['type'] == 'disorder_1' || $_POST['type'] == 'disorder_2' || $_POST['type'] == 'disorder_3'){
+	
+		switch ($_POST['type']) {
+			case 'disorder_1':
+				$position = 1;
+				break;
+				case 'disorder_2':
+				$position = 2;
+				break;
+				case 'disorder_3':
+				$position = 3;
+				break;
+		}
+	$value = $_POST['value'];
+	
+	$disorder = mysqli_query($con, "SELECT id FROM fighting_disroder WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."'  AND TYPE ='disorder'");  
+	
+	if($disorder->num_rows == 0){
+		$sql = "INSERT INTO fighting_disroder (ID_SURVIVOR, TYPE, ID_ACTION,POSITION) VALUES (".$survivor_id.",'disorder', ".$value.", ".$position.")";
+		
+	}else{
+		$sql = "UPDATE fighting_disroder SET ID_ACTION ='".$value."' WHERE ID_SURVIVOR='".$survivor_id."' AND POSITION ='".$position."' AND TYPE ='disorder'";
+		
+	}
+	
+	
+		if ($con->query($sql) === TRUE) {
+			error_log('sucess');
+		} else {
+			error_log( "Error updating record: " . $con->error);
+		}
+	
+		$survivor_id = $_POST['id'];
+		$disorder_description1 = mysqli_fetch_assoc(mysqli_query($con, "SELECT Rules FROM disorder WHERE ID = $value"));
+
+		echo'<textarea class="form-control" id="disorder_1" rows="3">'.(isset($disorder_description1['Rules']) ? $disorder_description1['Rules'] : '').'</textarea>';
+
+	}
 ?>
 <script>
           $(document).ready(function() {
